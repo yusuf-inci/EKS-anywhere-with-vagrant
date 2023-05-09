32fd18b317bad6ab250f59a74e0cd0b24e87c439
yusuf-inci <yusufinci@protonmail.com>
2023-05-09 11:24:56 +0300

    added Using a ConfigMap to store configuration

--- ContainersAndDocker/app.js
+++ ContainersAndDocker/app.js
@@ -4,7 +4,9 @@ var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello world from ' + hostname + '! Your app is up and running!\n')
+  // res.send('Hello world from ' + hostname + '! Your app is up and running!\n')
+  // res.send('Welcome to ' + hostname + '! Your app is up and running!\n')
+  res.send(process.env.MESSAGE + '\n')
 })
 app.listen(8080, function() {
   console.log('Sample app is listening on port 8080.')
